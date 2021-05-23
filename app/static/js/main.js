23c88f49c305180c26d51ac6eff6c1d8bdbf8a1d
sh15h4nk <shiva.shvvs@gmail.com>
2021-05-24 04:09:20 +0530

    added base.html and linked up all the pages in the nav

---
+++ app/static/js/main.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById("loader").style.display = "none";
+    }, 3500);
+    document.getElementById("hero").style.height = window.innerHeight + "px"; //window.innerHeight + "px";
+    };
+
+    // reset the height whenever the window's resized
+    //window.addEventListener("resize", resetHeight());
+    console.log(window.innerHeight);
+
+
+
+
---
+++ creds.py
@@ -0,0 +1,9 @@
+#app
+secret_key = "l1ckm41b411s"
+
+
+#AWS RDS SQL CREDS
+dbuser="t3ckz1t3"
+dbpasswd="$_MUSK_dumps_$b1tc01n"
+dbhost="teckzite-db.c9nqgqyhxotq.us-east-2.rds.amazonaws.com"
+dbname="tz2021"
