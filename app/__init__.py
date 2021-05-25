39505bd12e149ddf5b939adf8e72c76514f29a54
jokr <jokrhub>
2021-05-25 23:19:53 +0530

    update oauth

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_session import Session
 from flask_mail import Mail
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -14,7 +13,6 @@ bcrypt = Bcrypt(app)
 
 
 app.config['SESSION_SQLALCHEMY'] = db
-Session(app)
 
 from app.controllers import *
 
