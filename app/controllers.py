2a6da646cf1b6a74fb404864a229e0a63711f1c6
jokr <jokrhub>
2021-05-25 11:16:02 +0530

    revert

--- app/controllers.py
+++ app/controllers.py
@@ -4,110 +4,113 @@ from app.models import TechUser
 from app.functions import *
 from creds import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
 
-#oauth
-from flask_login import (
-	LoginManager,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
-)
+# #oauth
+# from flask_login import (
+# 	LoginManager,
+#     current_user,
+#     login_required,
+#     login_user,
+#     logout_user,
+# )
 
-from oauthlib.oauth2 import WebApplicationClient
-import requests, json
+# from oauthlib.oauth2 import WebApplicationClient
+# import requests, json
 
-client = WebApplicationClient(GOOGLE_CLIENT_ID)
+# client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
 
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return TechUser.query.get(user_id)
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return TechUser.query.get(user_id)
 
-@login_manager.unauthorized_handler
-def unauthorized():
-    flash("You must login ")
-    return redirect(url_for('index'))
+# @login_manager.unauthorized_handler
+# def unauthorized():
+#     flash("You must login ")
+#     return redirect(url_for('index'))
 
+
+
+# @app.route('/')
+# def temp():
+# 	if 'id' in session:
+# 		return ("You are authenticaed")
+# 	else:
+# 		return ("You are not authenticaed")
+
+
+
+# @app.route("/login", methods=['GET', 'POST'])
+# def login():
+ 
+#     google_provider_cfg = get_google_provider_cfg()
+#     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+#     request_uri = client.prepare_request_uri(
+#         authorization_endpoint,
+#         redirect_uri=request.base_url + "/callback",
+#         scope=["openid", "email", "profile"],
+#     )
+#     return redirect(request_uri)
+
+
+# @app.route("/login/callback")
+# def callback():
+
+# 	code = request.args.get("code")
+
+# 	google_provider_cfg = get_google_provider_cfg()
+# 	token_endpoint = google_provider_cfg["token_endpoint"]
+
+# 	token_url, headers, body = client.prepare_token_request(
+# 		token_endpoint,
+# 		authorization_response=request.url,
+# 		redirect_url=request.base_url,
+# 		code=code
+# 	)
+# 	token_response = requests.post(
+# 		token_url,
+# 		headers=headers,
+# 		data=body,
+# 		auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+# 	)
+
+# 	client.parse_request_body_response(json.dumps(token_response.json()))
+
+# 	userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+# 	uri, headers, body = client.add_token(userinfo_endpoint)
+# 	userinfo_response = requests.get(uri, headers=headers, data=body)
+
+# 	if userinfo_response.json().get("email_verified"):
+# 	    unique_id = userinfo_response.json()["sub"]
+# 	    users_email = userinfo_response.json()["email"]
+# 	    picture = userinfo_response.json()["picture"]
+# 	    users_name = userinfo_response.json()["given_name"]
+# 	else:
+# 	    return "User email not available or not verified by Google.", 400
+
+
+# 	user = TechUser.query.filter_by(userId=unique_id).first()
+
+# 	if not user:
+# 		new_user = TechUser(userId=unique_id, name=users_name, email=users_email)
+# 		db.session.add(new_user)
+# 		db.session.commit()
+	    
+# 	session['id'] = 1
+
+# 	# if not user.registration_status:
+# 	# 	return redirect(url_for('register'))
+
+# 	return redirect(url_for("temp"))
 
 
 @app.route('/')
 def temp():
-	if 'id' in session:
-		return ("You are authenticaed")
-	else:
-		return ("You are not authenticaed")
-
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
- 
-    google_provider_cfg = get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
-
-    request_uri = client.prepare_request_uri(
-        authorization_endpoint,
-        redirect_uri=request.base_url + "/callback",
-        scope=["openid", "email", "profile"],
-    )
-    return redirect(request_uri)
-
-
-@app.route("/login/callback")
-def callback():
-
-	code = request.args.get("code")
-
-	google_provider_cfg = get_google_provider_cfg()
-	token_endpoint = google_provider_cfg["token_endpoint"]
-
-	token_url, headers, body = client.prepare_token_request(
-		token_endpoint,
-		authorization_response=request.url,
-		redirect_url=request.base_url,
-		code=code
-	)
-	token_response = requests.post(
-		token_url,
-		headers=headers,
-		data=body,
-		auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
-	)
-
-	client.parse_request_body_response(json.dumps(token_response.json()))
-
-	userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-	uri, headers, body = client.add_token(userinfo_endpoint)
-	userinfo_response = requests.get(uri, headers=headers, data=body)
-
-	if userinfo_response.json().get("email_verified"):
-	    unique_id = userinfo_response.json()["sub"]
-	    users_email = userinfo_response.json()["email"]
-	    picture = userinfo_response.json()["picture"]
-	    users_name = userinfo_response.json()["given_name"]
-	else:
-	    return "User email not available or not verified by Google.", 400
-
-
-	user = TechUser.query.filter_by(userId=unique_id).first()
-
-	if not user:
-		new_user = TechUser(userId=unique_id, name=users_name, email=users_email)
-		db.session.add(new_user)
-		db.session.commit()
-	    
-	session['id'] = 1
-
-	# if not user.registration_status:
-	# 	return redirect(url_for('register'))
-
-	return redirect(url_for("temp"))
-
-
+	return Response()
 	
 
 @app.route('/home')
