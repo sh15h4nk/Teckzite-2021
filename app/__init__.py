e88d9dc1aaac2a607552c46bac1e8f40e4309a46
jokr <jokrhub>
2021-05-26 17:01:44 +0530

    added registrations

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 
 app = Flask(__name__)
@@ -14,7 +15,10 @@ bcrypt = Bcrypt(app)
 
 app.config['SESSION_SQLALCHEMY'] = db
 
+
 from app.controllers import *
+
+
 
 
 
--- app/functions.py
+++ app/functions.py
@@ -10,7 +10,41 @@ def getEvents(eventId='all'):
 	event = Event.query.filter_by(hidden=0, eventId=eventId).first()
 	return event
 
+def addAddress(t_userId, address_data):
+	address = Address(address_data)
+	address.t_userId = t_userId
+	db.session.add(address)
+	db.session.commit()
+
+	return address
+
+def addUser(userId, data):
+	del data['state']
+	del data['country']
+	del data['city']
+
+	user = TechUser.query.filter_by(userId=userId).first()
+	if not user:
+		return
+
+	user.update(data)
+	db.session.commit()
+
+	return user
+
+def addRguktUser(userId, data):
+	
+	user = TechUser.query.filter_by(userId=userId).first()
+	if not user:
+		return
+
+	user.update(data)
+	db.session.commit()
+
+	return user
 
 
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+
