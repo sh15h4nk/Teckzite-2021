8213e0a839017a24d758ca475b4371a2159065b4
sh15h4nk <shiva.shvvs@gmail.com>
2021-05-28 01:08:31 +0530

    Updated required to verify the reg stat

--- app/controllers.py
+++ app/controllers.py
@@ -40,102 +40,6 @@ def noindex():
 	return r
 
 
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-
-	if current_user.is_authenticated:
-		if not current_user.registration_status:
-
-			if current_user.email.endswith("rguktn.ac.in"):
-				return redirect(url_for('registerRgukt'))
-			else:
-				return redirect(url_for('registerUser'))
-
-		else:
-			flash("Already logged in")
-			return redirect(url_for('index'))
-
-	google_provider_cfg = get_google_provider_cfg()
-	authorization_endpoint = google_provider_cfg["authorization_endpoint"]
-
-	request_uri = client.prepare_request_uri(authorization_endpoint,redirect_uri=request.base_url + "/callback",scope=["openid", "email", "profile"])
-	return redirect(request_uri)
-
-
-@app.route("/login/callback")
-def callback():
-
-	code = request.args.get("code")
-
-	google_provider_cfg = get_google_provider_cfg()
-	token_endpoint = google_provider_cfg["token_endpoint"]
-
-	token_url, headers, body = client.prepare_token_request(
-		token_endpoint,
-		authorization_response=request.url,
-		redirect_url=request.base_url,
-		code=code
-	)
-	token_response = requests.post(
-		token_url,
-		headers=headers,
-		data=body,
-		auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
-	)
-
-	client.parse_request_body_response(json.dumps(token_response.json()))
-
-	userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-	uri, headers, body = client.add_token(userinfo_endpoint)
-	userinfo_response = requests.get(uri, headers=headers, data=body)
-
-	if userinfo_response.json().get("email_verified"):
-	    unique_id = userinfo_response.json()["sub"]
-	    users_email = userinfo_response.json()["email"]
-	    picture = userinfo_response.json()["picture"]
-	    users_name = userinfo_response.json()["given_name"]
-	else:
-	    return "User email not available or not verified by Google.", 400
-
-
-	user = TechUser.query.filter_by(userId=unique_id).first()
-
-	# if user logins first time with this email, add user to the database and then login the user
-	if not user:
-		new_user = TechUser(userId=unique_id, name=users_name, email=users_email)
-		db.session.add(new_user)
-		db.session.commit()
-
-		login_user(new_user)
-		flash("Logged in successfully")
-		
-
-	# if user is already logged in with this email, just login the user
-	else:
-		login_user(user)
-		flash("Logged in successfully")
-	    
-
-	# check if user registration is complete
-	if not current_user.registration_status:
-
-		if current_user.email.endswith("rguktn.ac.in"):
-			return redirect(url_for('registerRgukt'))
-		else:
-			return redirect(url_for('registerUser'))
-
-
-	return redirect(url_for("index"))
-
-@app.route('/logout')
-def logout():
-	logout_user()
-	flash("You have been logged out")
-	return redirect(url_for('index'))
-
-
-
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -200,51 +104,186 @@ def teamView():
 def devteamView():
 	return render_template('web_team.html')
 
-@app.route('/register.rgukt', methods=['GET', 'POST'])
-@login_required
-def registerRgukt():
 
-	if request.method == 'POST':
-		
-		try:
-			user = addRguktUser(current_user.userId, request.form)
-			flash("Your details have been added successfully")
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+
+	if current_user.is_authenticated:
+		if not current_user.registration_status:
+			return redirect(url_for('register'))
+			# if current_user.email.endswith("rguktn.ac.in"):
+			# 	return redirect(url_for('registerRgukt'))
+			# else:
+			# 	return redirect(url_for('registerUser'))
+
+		else:
+			flash("Already logged in")
 			return redirect(url_for('index'))
 
-		except Exception as e:
-			raise e
+	google_provider_cfg = get_google_provider_cfg()
+	authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+	request_uri = client.prepare_request_uri(authorization_endpoint,redirect_uri=request.base_url + "/callback",scope=["openid", "email", "profile"])
+	return redirect(request_uri)
 
 
-	return render_template('register_rgukt.html')
+@app.route("/login/callback")
+def callback():
+
+	code = request.args.get("code")
+
+	google_provider_cfg = get_google_provider_cfg()
+	token_endpoint = google_provider_cfg["token_endpoint"]
+
+	token_url, headers, body = client.prepare_token_request(
+		token_endpoint,
+		authorization_response=request.url,
+		redirect_url=request.base_url,
+		code=code
+	)
+	token_response = requests.post(
+		token_url,
+		headers=headers,
+		data=body,
+		auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+	)
+
+	client.parse_request_body_response(json.dumps(token_response.json()))
+
+	userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+	uri, headers, body = client.add_token(userinfo_endpoint)
+	userinfo_response = requests.get(uri, headers=headers, data=body)
+
+	if userinfo_response.json().get("email_verified"):
+	    unique_id = userinfo_response.json()["sub"]
+	    users_email = userinfo_response.json()["email"]
+	    picture = userinfo_response.json()["picture"]
+	    users_name = userinfo_response.json()["given_name"]
+	else:
+	    return "User email not available or not verified by Google.", 400
 
 
-@app.route('/register.user', methods=['GET', 'POST'])
+	user = TechUser.query.filter_by(userId=unique_id).first()
+
+	# if user logins first time with this email, add user to the database
+	if not user:
+		user = TechUser(userId=unique_id, name=users_name, email=users_email)
+		db.session.add(user)
+		db.session.commit()		
+
+	login_user(user)
+	flash("Logged in successfully")
+	    
+
+	# check if user registration is complete
+	if not current_user.registration_status:
+		return redirect(url_for('register'))
+		# if current_user.email.endswith("rguktn.ac.in"):
+		# 	return redirect(url_for('registerRgukt'))
+		# else:
+		# 	return redirect(url_for('registerUser'))
+
+
+	return redirect(url_for("index"))
+
+@app.route('/logout')
 @login_required
-def registerUser():
+def logout():
+	logout_user()
+	flash("You have been logged out")
+	return redirect(url_for('index'))
 
-	if request.method == 'POST':
+
+# @app.route('/register.rgukt', methods=['GET', 'POST'])
+# @login_required
+# def registerRgukt():
+
+# 	if request.method == 'POST':
 		
-		address_data = {}
-		address_data['state'] = request.form['state']
-		address_data['district'] = request.form['district']
-		address_data['city'] = request.form['city']
+# 		try:
+# 			user = addRguktUser(current_user.userId, request.form)
+# 			flash("Your details have been added successfully")
+# 			return redirect(url_for('index'))
 
-		user_data = dict(request.form)
-
-		try:
-			user = addUser(current_user.userId, user_data)
-			address = addAddress(user.userId, address_data)
-			flash("Your details have been added successfully")
-			return redirect(url_for('index'))
-
-		except Exception as e:
-			raise e
+# 		except Exception as e:
+# 			raise e
 
 
+# 	return render_template('register_rgukt.html')
 
-	return render_template('register_user.html')
 
+# @app.route('/register.user', methods=['GET', 'POST'])
+# @login_required
+# def registerUser():
+
+# 	if request.method == 'POST':
+		
+# 		address_data = {}
+# 		address_data['state'] = request.form['state']
+# 		address_data['district'] = request.form['district']
+# 		address_data['city'] = request.form['city']
+
+# 		user_data = dict(request.form)
+
+# 		try:
+# 			user = addUser(current_user.userId, user_data)
+# 			address = addAddress(user.userId, address_data)
+# 			flash("Your details have been added successfully")
+# 			return redirect(url_for('index'))
+
+# 		except Exception as e:
+# 			raise e
+
+
+
+# 	return render_template('register_user.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+@login_required
+def register():
+	if request.method == "POST":
+		if len(set(["payment_status", "userId", "email", "registration_status","tzID", "hidden"]) & set(request.form.keys())) != 0:
+			return "Invalid Data"
+
+		elif current_user.email.endswith("rguktn.ac.in"):
+			try:
+				user = addRguktUser(current_user.userId, request.form)
+				flash("Your details have been added successfully")
+				return redirect(url_for('index'))
+
+			except Exception as e:
+				raise e
+		
+		else:
+			if not request.form['state'] or not request.form['city'] or not request.form['district']:
+				flash("Missing Required Fields")
+				return render_template('register_user.html')
+			address_data = {}
+			address_data['state'] = request.form['state']
+			address_data['district'] = request.form['district']
+			address_data['city'] = request.form['city']
+
+			user_data = dict(request.form)
+
+			try:
+				user = addUser(current_user.userId, user_data)
+				address = addAddress(user.userId, address_data)
+				flash("Your details have been added successfully")
+				return redirect(url_for('index'))
+
+			except Exception as e:
+				raise e
+
+
+	#for get requests
+	elif current_user.email.endswith("rguktn.ac.in"):
+		return render_template('register_rgukt.html')
+	else:
+		return render_template('register_user.html')
 
 @app.route('/profile')
 def profile():
+	# registration_stat()
+	if current_user.is_authenticated and not current_user.registration_status:
+		return redirect(url_for('register'))
 	return render_template('userProfile.html')
--- app/functions.py
+++ app/functions.py
@@ -1,6 +1,8 @@
 from app.models import *
 import requests
 from creds import GOOGLE_DISCOVERY_URL
+from flask_login import current_user
+from flask import redirect, url_for
 
 def getEvents(eventId='all'):
 	if eventId == 'all':
@@ -23,7 +25,7 @@ def addUser(userId, data):
 	del data['district']
 	del data['city']
 
-	user = TechUser.query.filter_by(userId=userId)
+	user = TechUser.query.filter_by(userId=userId).first()
 	if not user:
 		return
 
@@ -31,11 +33,11 @@ def addUser(userId, data):
 	user.update({'registration_status': 1})
 	db.session.commit()
 
-	return user.first()
+	return user
 
 def addRguktUser(userId, data):
 	
-	user = TechUser.query.filter_by(userId=userId)
+	user = TechUser.query.filter_by(userId=userId).first()
 	if not user:
 		return
 
@@ -43,10 +45,22 @@ def addRguktUser(userId, data):
 	user.update({'registration_status': 1})
 	db.session.commit()
 
-	return user.first()
+	return user
 
 
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 
+def reg_stat(func):
+	@wraps(func)
+	def decorated_function(*args, **kwargs):
+		if current_user.is_authenticated and not current_user.registration_status:
+			return redirect(url_for('register'))
+		else:
+			return func(*args, **kwargs)
+	return decorated_function
+
+def registration_stat():
+	if current_user.is_authenticated and not current_user.registration_status:
+		return redirect(url_for('register'))
