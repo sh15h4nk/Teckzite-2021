0ebdc57f7a02d97ef4a069a8412df1d127cc2e35
jokr <jokrhub>
2021-05-28 13:13:17 +0530

    update registration

--- app/functions.py
+++ app/functions.py
@@ -3,6 +3,9 @@ import requests
 from creds import GOOGLE_DISCOVERY_URL
 from flask_login import current_user
 from flask import redirect, url_for
+from functools import wraps
+
+
 
 def getEvents(eventId='all'):
 	if eventId == 'all':
@@ -13,7 +16,7 @@ def getEvents(eventId='all'):
 	return event
 
 def addAddress(t_userId, address_data):
-	address = Address(address_data['state'], address_data['district'], address_data['city'])
+	address = Address(address_data['state'], address_data['district'], address_data['city'], address_data['pin'])
 	address.t_userId = t_userId
 	db.session.add(address)
 	db.session.commit()
@@ -24,8 +27,9 @@ def addUser(userId, data):
 	del data['state']
 	del data['district']
 	del data['city']
+	del data['pin']
 
-	user = TechUser.query.filter_by(userId=userId).first()
+	user = TechUser.query.filter_by(userId=userId)
 	if not user:
 		return
 
@@ -33,11 +37,11 @@ def addUser(userId, data):
 	user.update({'registration_status': 1})
 	db.session.commit()
 
-	return user
+	return user.first()
 
 def addRguktUser(userId, data):
 	
-	user = TechUser.query.filter_by(userId=userId).first()
+	user = TechUser.query.filter_by(userId=userId)
 	if not user:
 		return
 
@@ -45,22 +49,54 @@ def addRguktUser(userId, data):
 	user.update({'registration_status': 1})
 	db.session.commit()
 
-	return user
+	return user.first()
 
 
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 
-def reg_stat(func):
+
+
+
+def generate_techzite_id():
+
+    currentId = CurrentId.query.first()
+    current_techzite_id = currentId.current_techzite_id
+    
+    currentId.current_techzite_id += 1
+    db.session.commit()
+    
+    return "TZ"+str(current_techzite_id)
+
+def generate_event_id():
+
+    currentId = CurrentId.query.first()
+    current_event_id = currentId.current_event_id
+    
+    currentId.current_event_id += 1
+    db.session.commit()
+
+    return "EV"+str(current_event_id)
+
+def generate_workshop_id():
+
+    currentId = CurrentId.query.first()
+    current_workshop_id = currentId.current_workshop_id
+    
+    currentId.current_workshop_id += 1
+    db.session.commit()
+
+    return "WS"+str(current_workshop_id)
+
+
+def registration_required(func):
 	@wraps(func)
 	def decorated_function(*args, **kwargs):
+		print("trig")
 		if current_user.is_authenticated and not current_user.registration_status:
 			return redirect(url_for('register'))
 		else:
 			return func(*args, **kwargs)
 	return decorated_function
 
-def registration_stat():
-	if current_user.is_authenticated and not current_user.registration_status:
-		return redirect(url_for('register'))
---
+++ set.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=run;
+export FLASK_ENV=development;
+export OAUTHLIB_INSECURE_TRANSPORT=1;
