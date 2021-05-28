fd32f8fc6183842e7d74b6788389b029a69c12f8
Captain272 <www.abhijeeeth@gmail.com>
2021-05-28 12:15:57 +0530

    done 500

--- app/controllers.py
+++ app/controllers.py
@@ -170,3 +170,7 @@ def ca_portal():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_error(error):
+	return render_template('500.html'), 500
+
