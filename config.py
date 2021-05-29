ebf227d3ce41e2d1f3ed36aaec44a115ee1606bc
jokr <jokrhub>
2021-05-29 18:29:54 +0530

    done payments

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ DEGUG = True
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-# conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(creds.dbuser, creds.dbpasswd, creds.dbhost, creds.dbname)
-# SQLALCHEMY_DATABASE_URI = conn
+conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(creds.dbuser, creds.dbpasswd, creds.dbhost, creds.dbname)
+SQLALCHEMY_DATABASE_URI = conn
 
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'app.db')
+# SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'app.db')
 
 SESSION_TYPE = 'sqlalchemy'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -22,3 +22,7 @@ THREADS_PER_PAGE = 2
 
 
 SECRET_KEY = creds.secret_key
+
+
+S3_BUCKET                 = "tzimageupload"
+S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
