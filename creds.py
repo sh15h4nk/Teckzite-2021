fbc05d9a8f36795fea55e465ac99c1045ea8e33a
jokr <jokrhub>
2021-05-30 01:46:33 +0530

    fuck frontend

--- creds.py
+++ creds.py
@@ -8,8 +8,17 @@ dbpasswd="$_MUSK_dumps_$b1tc01n"
 dbhost="teckzite-db.c9nqgqyhxotq.us-east-2.rds.amazonaws.com"
 dbname="tz2021"
 
+# localhost
 GOOGLE_CLIENT_ID = "1013131542861-1uq8o1c9vbqbo7ss7kppaher9f4aud7g.apps.googleusercontent.com"
 GOOGLE_CLIENT_SECRET = "coxItyfUBEUMOtKQu7c4APlO"
 GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
+
+
+# for teckzite.org
+# GOOGLE_CLIENT_ID = "1013131542861-0st7doortm945a7gm08r2dbvr6hp52d7.apps.googleusercontent.com"
+# GOOGLE_CLIENT_SECRET = "vXA4wThrmrr1otedrjenWF37"
+# GOOGLE_DISCOVERY_URL = (
+#     "https://accounts.google.com/.well-known/openid-configuration"
+# )
