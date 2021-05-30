952dc9c0c7fea68f166daefad5b190fd9748cfb4
jokr <jokrhub>
2021-05-30 19:03:23 +0530

    final beta launch

--- creds.py
+++ creds.py
@@ -9,19 +9,19 @@ dbhost="teckzite-db.c9nqgqyhxotq.us-east-2.rds.amazonaws.com"
 dbname="tz2021"
 
 # localhost
-GOOGLE_CLIENT_ID = "1013131542861-1uq8o1c9vbqbo7ss7kppaher9f4aud7g.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "coxItyfUBEUMOtKQu7c4APlO"
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
-
-
-# for beta.teckzite.org
-# GOOGLE_CLIENT_ID = "1013131542861-ood3b607a52duf819o8i11ruapjlntbr.apps.googleusercontent.com"
-# GOOGLE_CLIENT_SECRET = "JPORWgAexAgNufJv-CH_K9hV"
+# GOOGLE_CLIENT_ID = "1013131542861-1uq8o1c9vbqbo7ss7kppaher9f4aud7g.apps.googleusercontent.com"
+# GOOGLE_CLIENT_SECRET = "coxItyfUBEUMOtKQu7c4APlO"
 # GOOGLE_DISCOVERY_URL = (
 #     "https://accounts.google.com/.well-known/openid-configuration"
 # )
+
+
+# for beta.teckzite.org
+GOOGLE_CLIENT_ID = "1013131542861-ood3b607a52duf819o8i11ruapjlntbr.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "JPORWgAexAgNufJv-CH_K9hV"
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 
 
 # for teckzite.org
