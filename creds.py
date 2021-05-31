b7e377cef7a5c497724f9f4f454ddb4dd3296fec
jokr <jokrhub>
2021-05-31 11:20:09 +0530

    comment oauth

--- creds.py
+++ creds.py
@@ -17,19 +17,19 @@ dbname="tz2021"
 
 
 # for beta.teckzite.org
-GOOGLE_CLIENT_ID = "1013131542861-ood3b607a52duf819o8i11ruapjlntbr.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "JPORWgAexAgNufJv-CH_K9hV"
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
-
-
-# for teckzite.org
-# GOOGLE_CLIENT_ID = "1013131542861-0st7doortm945a7gm08r2dbvr6hp52d7.apps.googleusercontent.com"
-# GOOGLE_CLIENT_SECRET = "vXA4wThrmrr1otedrjenWF37"
+# GOOGLE_CLIENT_ID = "1013131542861-ood3b607a52duf819o8i11ruapjlntbr.apps.googleusercontent.com"
+# GOOGLE_CLIENT_SECRET = "JPORWgAexAgNufJv-CH_K9hV"
 # GOOGLE_DISCOVERY_URL = (
 #     "https://accounts.google.com/.well-known/openid-configuration"
 # )
+
+
+# for teckzite.org
+GOOGLE_CLIENT_ID = "1013131542861-0st7doortm945a7gm08r2dbvr6hp52d7.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "vXA4wThrmrr1otedrjenWF37"
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 
 
 #Email server
