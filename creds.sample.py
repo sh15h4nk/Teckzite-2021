4494569c39e1be0084164ebb8035fb2d4c550bc4
sh15h4nk <shiva.shvvs@gmail.com>
2021-08-23 22:00:15 +0530

    added creds sample
    Signed-off-by: sh15h4nk <shiva.shvvs@gmail.com>

---
+++ creds.sample.py
@@ -0,0 +1,42 @@
+
+#app
+secret_key = ""
+
+
+#AWS RDS SQL CREDS (yours db connection creds)
+dbuser=""
+dbpasswd=""
+dbhost=""
+dbname=""
+
+# local db
+# dbuser="root"
+# dbpasswd="XXXXX"
+# dbhost="localhost"
+# dbname="tz2021"
+
+
+# localhost (congifuration of oauth)
+# GOOGLE_CLIENT_ID = ""
+# GOOGLE_CLIENT_SECRET = ""
+# GOOGLE_DISCOVERY_URL = (
+#     ""
+# )
+
+
+
+
+# for teckzite.org (configutation of google oauth)
+GOOGLE_CLIENT_ID = ""
+GOOGLE_CLIENT_SECRET = ""
+GOOGLE_DISCOVERY_URL = (
+    ""
+)
+
+
+#Email server
+email_username = "your username"
+email_passwd = "your password"
+
+mail_server = "your mail server"
+mail_port = "you mail port"
